05797b783fbe41c4f723634402c07883de147d0c
Aum Shah <101273167+Aum-Shah@users.noreply.github.com>
2024-05-24 06:03:25 +0530

    Update TelegramBotAPI.js

--- src/TelegramBotAPI.js
+++ src/TelegramBotAPI.js
@@ -32,7 +32,7 @@ export default class TelegramBotAPI {
         await this.callApi('sendMessage', {
             chat_id: chatId,
             text: text,
-            parse_mode: "Markdown",
+            parse_mode: "HTML",
             disable_web_page_preview:true,
             ...(inlineKeyboard && { reply_markup: { inline_keyboard: inlineKeyboard } })
         });
